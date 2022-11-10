a0f4a7ea52a44a6aef4306b5a1ff89df0109510f
Rupert Smith <rupert@thesett.com>
2022-11-10 16:11:27 +0000

    Implemented ZoomSpace.

--- src/elm/Camera2d.elm
+++ src/elm/Camera2d.elm
@@ -1,11 +1,13 @@
 module Camera2d exposing
     ( Camera2d
-    , zoomedAt
+    , zoomedAt, fromZoomSpace
     , origin, zoom
     , setOrigin, setZoom, setZoomAtScreenPoint
     , translateBy, translateByScreenVector
     , pointToScene, pointToScreen
     , svgViewBox
+    , ZoomSpace
+    , toZoomSpace
     )
 
 {-| A Camera2d maps a 2d scene to a screen.
@@ -31,7 +33,7 @@ addition of the zoom ratio.
 
 # Constructors
 
-@docs zoomedAt
+@docs zoomedAt, fromZoomSpace
 
 
 # Properties
@@ -50,15 +52,38 @@ addition of the zoom ratio.
 @docs pointToScene, pointToScreen
 @docs svgViewBox
 
+
+# Animation
+
+@docs ZoomSpace
+@docs toZoomSpace
+
 -}
 
 import Frame2d exposing (Frame2d)
 import Point2d exposing (Point2d)
-import Quantity exposing (Quantity, Rate)
+import Point3d exposing (Point3d)
+import Quantity exposing (Quantity, Rate, Unitless)
 import Rectangle2d exposing (Rectangle2d)
 import TypedSvg.Attributes
 import TypedSvg.Core
 import Vector2d exposing (Vector2d)
+
+
+{-| ZoomSpace is a mapping of the camera coordinates from (X, Y, Zoom) to
+(X, Y, 1 / Zoom). Linear motion of the camera in ZoomSpace maps to linear motion
+of the camera in the 3d space above the drawing.
+
+The camera can be thought of as existing at a Z height above a drawing on the (X, Y)
+plane at Z = 0, with the camera having a fixed field of view and looking straight
+down at the drawing, and always remaining in perfect focus. ZoomSpace is linear with
+respect to this 3d space above the drawing. Linear motion of the camera in ZoomSpace
+will produice linear motion of the camera as imagined this way, and is useful for
+producing predictable animations of the camera position and zoom level.
+
+-}
+type ZoomSpace screenUnits coordinates
+    = ZoomSpace
 
 
 {-| A Camera onto a 2d scene, centered on a particular origin point in the scene
@@ -82,6 +107,36 @@ zoomedAt originPoint zoomLevel =
     , sceneFrame = Frame2d.atPoint originPoint
     }
         |> Camera2d
+
+
+{-| Maps ZoomSpace to camera coords.
+-}
+fromZoomSpace : Point3d units (ZoomSpace screenUnits coordinates) -> Camera2d units screenUnits coordinates
+fromZoomSpace zoomSpacePoint =
+    let
+        ( x, y, z ) =
+            zoomSpacePoint
+                |> Point3d.coordinates
+
+        zoomLevel =
+            Quantity.at_ (Quantity.unsafe 1.0) z
+    in
+    zoomedAt (Point2d.xy x y) zoomLevel
+
+
+{-| Maps the camera coords to ZoomSpace.
+-}
+toZoomSpace : Camera2d units screenUnits coordinates -> Point3d units (ZoomSpace screenUnits coordinates)
+toZoomSpace (Camera2d { sceneFrame, zoomLevel }) =
+    let
+        ( x, y ) =
+            Frame2d.originPoint sceneFrame
+                |> Point2d.coordinates
+
+        z =
+            Quantity.at_ zoomLevel (Quantity.unsafe 1.0)
+    in
+    Point3d.xyz x y z
 
 
 {-| Gets the camera origin point in scene coordinates.
